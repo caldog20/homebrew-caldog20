d5fa7fe9cb8c99aedaf5aabec463e9558dbddc23
Caleb <caldog20@gmail.com>
2021-12-10 12:22:16 -0600

    updated repo name

--- aarch64-unknown-linux-gnu.rb
+++ aarch64-unknown-linux-gnu.rb
@@ -1,10 +1,10 @@
 class Aarch64UnknownLinuxGnu < Formula
   desc "aarch64 Linux GNU Toolchain"
-  homepage "https://github.com/caldog20/homebrew-formula"
+  homepage "https://github.com/caldog20/homebrew-caldog20"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
   version "8.3.0"
 
-  url "https://github.com/caldog20/homebrew-forumla/releases/download/v1.0.0/aarch64-unknown-linux-gnu.tar.gz"
+  url "https://github.com/caldog20/homebrew-caldog20/releases/download/v1.0.0/aarch64-unknown-linux-gnu.tar.gz"
   sha256 "bb24b4deaae63a61deb1d3eac02f24f4e226f507ee25e4cdefa467d7db6c10ec"
 
   def install
